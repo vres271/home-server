c416db8ac22017147dda4e4fc90057f53a7fbba3
vres271 <vres271@gmail.com>
2026-08-11 00:08:52 +0400

    Лого, иконка

--- orange-home-ui/src/app/features/dashboard/dashboard.component.ts
+++ orange-home-ui/src/app/features/dashboard/dashboard.component.ts
@@ -5,10 +5,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="p-4">
-      <h2>Добро пожаловать в OrangePi Hub</h2>
-      <p>Здесь будет мониторинг системы и быстрый доступ к функциям.</p>
+    <div class="m-4 orange-wrapper">
+      <img src="favicon.svg" alt="OrangePi Hub" width="100%"/>
+      <div class="hello">Дарова</div>
     </div>
-  `
+  `,
+  styles: `
+    .orange-wrapper {
+      position: relative;
+    }
+    .hello {
+      font-size: 3.5rem;
+      font-weight: 500;
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;      
+      color: var(--p-gray-100);
+    }
+  `,
 })
 export class DashboardComponent {}
